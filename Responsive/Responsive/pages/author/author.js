df157e092bc33022882c0e69a73d1eb66c9b51dc
Daniel C. Wilson <dwilson1@gmail.com>
2014-02-26 11:06:57 -0600

    final demo screen, now the media query fun starts...

---
+++ Responsive/Responsive/pages/author/author.js
@@ -0,0 +1,83 @@
+﻿(function () {
+    "use strict";
+
+    var nav = WinJS.Navigation;
+    var ui = WinJS.UI;
+
+    var _groups = {
+        earlier: '3',
+        weekAgo: '2',
+        thisWeek: '1'
+    }
+
+    ui.Pages.define("/pages/author/author.html", {
+        // This function is called to initialize the page.
+        init: function (element, options) {
+            if (options.author) {
+                this.pastries = WinJS.xhr({
+                    type: 'GET',
+                    url: 'https://the-pastry-box-project.net/api/v1/consume/thoughts?baker=' + options.author,
+                    responseType: 'json'
+                });
+            }
+        },
+
+        // This function is called whenever a user navigates to this page.
+        ready: function (element, options) {
+            this.element.querySelector(".titlearea .pagetitle").textContent = options.name;
+            this.listView = document.querySelector('.groupeditemslist').winControl;
+            this.listView.addEventListener('iteminvoked', this._itemInvoked.bind(this));
+
+            this.pastries.done(function (resp) {
+                this.stopProgress();
+                if (resp && resp.response && resp.response.length) {
+                    this.prepareList(resp.response);
+                } else {
+                    //none found
+                }
+            }.bind(this));
+        },
+        prepareList: function(response) {
+            var articles = JSON.parse(response);
+            for (var i = 0, length = articles.length; i < length; ++i) {
+                var current = articles[i];
+                current.group = 'allbyauthor';
+                if (!current.title) {
+                    current.title = current.nicedate;
+                }
+                if (current.photo) {
+                    current.photoUrl = 'url(' + current.photo + ')';
+                }
+            }
+            var articleList = new WinJS.Binding.List(articles);
+            this.listView.itemDataSource = articleList.dataSource;
+            this.groupedItems = articleList.createGrouped(this.getGroupKey, this.getGroupData);
+            this.listView.groupDataSource = this.groupedItems.groups.dataSource;
+        },
+        unload: function() {
+
+        },
+
+        updateLayout: function (element) {
+            /// <param name="element" domElement="true" />
+            document.querySelector('.groupeditemslist').winControl.recalculateItemPosition();
+            // TODO: Respond to changes in layout.
+        },
+
+        _itemInvoked: function (args) {
+            var item = this.groupedItems.getAt(args.detail.itemIndex);
+            nav.navigate("/pages/itemDetail/itemDetail.html", { item: item });
+        },
+        stopProgress: function () {
+
+        },
+        getGroupKey: function (data) {
+            return data.group;
+        },
+        getGroupData: function (data) {
+            return {
+                title: 'All Thoughts'
+            };
+        }
+    });
+})();
--- Responsive/Responsive/pages/itemDetail/itemDetail.js
+++ Responsive/Responsive/pages/itemDetail/itemDetail.js
@@ -1,6 +1,8 @@
 ﻿(function () {
     "use strict";
 
+    var nav = WinJS.Navigation;
+
     WinJS.UI.Pages.define("/pages/itemDetail/itemDetail.html", {
         // This function is called whenever a user navigates to this page. It
         // populates the page elements with the app's data.
@@ -11,14 +13,17 @@
             this.element = element;
             this.element.querySelector(".titlearea .pagetitle").textContent = this.item.title;
             this.prepareArticle();
-
+        },
+        selectAuthor: function() {
+            nav.navigate("/pages/author/author.html", { author: this.item.slug, name: this.item.realname  });
         },
         prepareArticle: function () {
             var article = this.element.querySelector(".main-article article");
             var author = document.createElement('div');
             var name = document.createElement('p');
+            var images;
 
-            article.innerHTML = '<div class="author"></div>' + this.item.thought;
+            article.innerHTML = '<div class="author" role="link"></div>' + this.item.thought;
             author.classList.add('details');
             author.appendChild(name);
 
@@ -27,6 +32,15 @@
 
             article.querySelector('.author').appendChild(author);
             author.parentNode.style.backgroundImage = 'url(' + this.item.photo + ')';
+            author.parentNode.addEventListener("click", this.selectAuthor.bind(this), false);
+
+            images = article.querySelectorAll('img:not([src])');
+            if (images && images.length) {
+                for (var i = 0, length = images.length; i < length; ++i) {
+                    var image = images[i];
+                    image.parentNode.removeChild(image);
+                }
+            }
         }
     });
 })();
